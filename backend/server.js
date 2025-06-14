5bcadbc49ea47e51ccb23cae3d6deb359c1ea1c8
hemanth0504 <hemanth.teegala5@gmail.com>
2025-06-14 01:39:41 -0400

    Create Server and Controllers

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
+
+const app = express(); 
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth",authRoutes);
+
+console.log(PORT);
+
+app.listen(3000,()=>{
+    console.log("Server is running on http://localhost:" + PORT)
+    connectDB();
+})
+
+//s64wH6gBbDwyjmzL
+
+//hemanthteegala5
