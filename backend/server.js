9bc0b5fcd34fadcd75177730cda80019ada3c07d
hemanth0504 <hemanth.teegala5@gmail.com>
2025-06-14 02:26:09 -0400

    Bcrypt setup

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ dotenv.config();
 
 const app = express(); 
 const PORT = process.env.PORT || 3000;
+app.use(express.json()); // allows to parse the body of the request
 
 app.use("/api/auth",authRoutes);
 
