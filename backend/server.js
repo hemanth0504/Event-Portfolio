14252b392914a7a32c279ae989d38c7337e74796
hemanth0504 <hemanth.teegala5@gmail.com>
2025-06-15 00:08:51 -0400

    Set up Cloudinary and stripe

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import authRoutes from "./routes/auth.route.js"
+import productRoutes from "./routes/product.route.js"
+import cartRoutes from "./routes/cart.route.js"
+import paymentRoutes from "./routes/payment.route.js"
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -15,6 +18,9 @@ app.use(cookieParser());
 
 
 app.use("/api/auth",authRoutes);
+app.use("/api/products",productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/payments",paymentRoutes);
 
 console.log(PORT);
 
