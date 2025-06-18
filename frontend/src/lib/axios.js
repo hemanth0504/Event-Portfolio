40a7f538aeabfd0fa33dd6769956c12254c09b20
hemanth0504 <hemanth.teegala5@gmail.com>
2025-06-18 13:47:32 -0400

    custom axios instance

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+console.log("✅ Using custom axios instance");
+
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:3000/api" : "/api",
-	withCredentials: true, 
+	baseURL: import.meta.env.DEV ? "http://localhost:3000/api" : "/api",
+	withCredentials: true,
 });
 
 export default axiosInstance;
