44b28c28e70f7197c2781c7bfdade461ed5c65f2
hemanth0504 <hemanth.teegala5@gmail.com>
2025-06-18 13:53:19 -0400

    refactor(auth) : refactor auth routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, signup, refreshToken } from "../controllers/auth.controller.js";
+import { login, logout, signup, refreshToken, getProfile } from "../controllers/auth.controller.js";
 
 
 const router = express.Router();
@@ -9,7 +9,8 @@ router.post("/signup",signup)
 router.post("/login",login)
 router.post("/logout",logout)
 router.post("/refresh-token",refreshToken);
-//router.get("/profile", getProfile);
+router.get("/me", getProfile);
+router.get("/profile", getProfile);
 
 
 
