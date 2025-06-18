77a3ad86ada0319fee03c9aca212c12e5b569dff
hemanth0504 <hemanth.teegala5@gmail.com>
2025-06-18 13:46:00 -0400

    Create User Store

--- frontend/src/context/useAuthContext.jsx
+++
@@ -1,5 +0,0 @@
-// src/hooks/useAuthContext.js
-import { useContext } from "react";
-import { AuthContext } from "./AuthProvider.jsx";
-
-export const useAuthContext = () => useContext(AuthContext);
